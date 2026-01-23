8f47c2158319ecffc0cccd999b2d2c106d1e0d0b
GrahamBrown-OregonState <browgrah@oregonstate.edu>
2026-01-22 19:00:42 -0800

    fix type again :(

--- lab1/part2.c
+++ lab1/part2.c
@@ -35,7 +35,7 @@ int main(void)
 while (1) // loop forever
       {
       // Read and extract only 4th and 5th bits
-            unint8_t mpr = PIND & 0b00110000;
+            uint8_t mpr = PIND & 0b00110000;
 
             if (mpr == 0b00100000) { // right whisker is hit
                   PORTB = MovBck;     // move backward
